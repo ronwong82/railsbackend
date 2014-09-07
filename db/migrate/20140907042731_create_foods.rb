e7a6d2ee4f1a04a1c9b1d604b380f01b68936b92
Ronald Wong <ronald.acer82@gmail.com>
2014-09-06 23:06:11 -0600

    Added food model

--- db/migrate/20140907042731_create_foods.rb
+++ db/migrate/20140907042731_create_foods.rb
@@ -1,7 +1,24 @@
 class CreateFoods < ActiveRecord::Migration
   def change
     create_table :foods do |t|
+      t.string :name, null: false
+      t.integer :points, null: false, default: 0
+      
+      # ext
+      t.string :description
+      t.integer :serving_size_numerator
+      t.integer :serving_size_denominator
+      t.string :serving_size_unit, default: "servings"
+      t.boolean :is_fruit_or_vegetable, default: false
+      t.boolean :is_using_nutrition_info, default: false
+      t.float :nutrition_fat, default: 0
+      t.float :nutrition_carbs, default: 0
+      t.float :nutrition_fiber, default: 0
+      t.float :nutrition_protein, default: 0
+      t.string :nutrition_unit, default: "grams"
 
+      #server side
+      t.string :remote_ip
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907032807) do
+ActiveRecord::Schema.define(version: 20140907042731) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,24 @@ ActiveRecord::Schema.define(version: 20140907032807) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "foods", force: true do |t|
+    t.string   "name",                                          null: false
+    t.integer  "points",                   default: 0,          null: false
+    t.string   "description"
+    t.integer  "serving_size_numerator"
+    t.integer  "serving_size_denominator"
+    t.string   "serving_size_unit",        default: "servings"
+    t.boolean  "is_fruit_or_vegetable",    default: false
+    t.boolean  "is_using_nutrition_info",  default: false
+    t.float    "nutrition_fat",            default: 0.0
+    t.float    "nutrition_carbs",          default: 0.0
+    t.float    "nutrition_fiber",          default: 0.0
+    t.float    "nutrition_protein",        default: 0.0
+    t.string   "nutrition_unit",           default: "grams"
+    t.string   "remote_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
